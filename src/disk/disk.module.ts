6f6bf6691d0d517baecae1b1b7ec8acfa30e2007
Mark Faust <markfaust123@gmail.com>
2024-09-05 07:24:08 -0400

    now allowing for dependency within power service to be injected into the cpu and disk modules\' services

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
+import { PowerService } from 'src/power/power.service';
 
 @Module({
+  imports: [PowerService],
   providers: [DiskService]
 })
 export class DiskModule {}
